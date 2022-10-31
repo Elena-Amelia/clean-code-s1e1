1a381a065499f54d1c7fbc86b761348b0bd6aa5a
Elena-Amelia <amelia013@ya.ru>
2022-10-31 21:21:00 +0400

    refactor: rename classes and id - rule 3.2 in html-and-css.md

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("addNewTask");//Add a new task.
+var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("toDoTasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("doneTasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("current-task");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("done-task");//completed-tasks
 
 
 //New task list item
